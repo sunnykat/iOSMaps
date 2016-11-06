1617fe562c55c4977b05ceaf74024091f23c6dee
Fiaz Sami <fiaz.sami@collecdev.com>
2016-11-06 12:11:43 -0800

    Initial Commit

---
+++ DojoMaps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DojoMaps
+//
+//  Created by Fiaz Sami on 11/6/16.
+//  Copyright © 2016 codingdojo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
